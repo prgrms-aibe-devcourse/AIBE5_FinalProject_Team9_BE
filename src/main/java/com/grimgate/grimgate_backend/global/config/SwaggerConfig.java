3c43e2d78c246bd94f9150772aee024b8e5c2113
unknown <sjwwiies2829@gmail.com>
2026-06-22 03:20:34 +0900

    feat: 백엔드 배포 워크플로우 추가 및 prod 환경 Swagger 비활성화

--- src/main/java/com/grimgate/grimgate_backend/global/config/SwaggerConfig.java
+++ src/main/java/com/grimgate/grimgate_backend/global/config/SwaggerConfig.java
@@ -6,9 +6,11 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 // Swgger 테스트 시 authorize 버튼 설정
 @Configuration
+@Profile("!prod")
 public class SwaggerConfig {
 
     @Bean
